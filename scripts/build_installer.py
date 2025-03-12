e7c6423edabf624999377977df7289774d29ad17
Mia Battad <133708990+miabatta@users.noreply.github.com>
2025-03-12 15:05:39 +0000

    fix(installer): Adapt installer script for prod builds
    Signed-off-by: Mia Battad <133708990+miabatta@users.noreply.github.com>

--- scripts/build_installer.py
+++ scripts/build_installer.py
@@ -82,8 +82,8 @@ class EvaluationBuildError(Exception):
     pass
 
 
-def download_from_s3(bucket_name: str, key: Path, output_folder: Path) -> Path:
-    dest_path = output_folder / key.name
+def download_from_s3(bucket_name: str, key: str, output_folder: str) -> Path:
+    dest_path = Path(output_folder) / Path(key).name
     print(f"Downloading {key} from s3:\\\\{bucket_name}")
     import boto3
 
@@ -124,7 +124,7 @@ def build_installer(
     else:
         install_builder_path = install_builder_location
 
-    if not install_builder_path.is_dir():
+    if not Path(install_builder_path).is_dir():
         raise FileNotFoundError(
             f"InstallBuilder path '{str(install_builder_path)}' must be a directory containing 'bin/builder'."
         )
@@ -138,27 +138,35 @@ def build_installer(
     date = datetime.today().date()
 
     print("Running Install Builder...")
-    output = subprocess.run(
-        [
-            install_builder,
-            "build",
-            INSTALLER_ROOT / INSTALLER_TEMPLATE,
-            platform,
-            "--setvars",
-            f"project.outputDirectory={out_dir}",
-            f"project.version={installer_version[:8]}-{date}",
-        ],
-        check=True,
-        capture_output=True,
-        text=True,
-    )
+    try:
+        output = subprocess.run(
+            [
+                install_builder,
+                "build",
+                INSTALLER_ROOT / INSTALLER_TEMPLATE,
+                platform,
+                "--setvars",
+                f"project.outputDirectory={out_dir}",
+                f"project.version={installer_version[:8]}-{date}",
+            ],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Encountered an error when running: {e.output}")
+        raise
     print(
         f"{'-'*30}\nBegin Install Builder Output\n{'-'*30}\n"
         f"{output.stdout}"
         f"{'-'*30}\nEnd Install Builder Output\n{'-'*30}\n"
     )
 
-    if EVALUATION_VERSION_STRING in output.stdout and not local_dev_build:
+    if (
+        EVALUATION_VERSION_STRING in output.stdout
+        and not local_dev_build
+        and license_file is not None
+    ):
         raise EvaluationBuildError(
             "InstallBuilder was detected using an evaluation version, which is only permitted in local dev builds."
         )
@@ -204,8 +212,7 @@ def main(args: argparse.Namespace) -> None:
 
         if components_dir.exists():
             shutil.rmtree(components_dir, onerror=_add_write_perms)
-        src_component_path = Path("./install_builder")
-        shutil.copytree(src_component_path, components_dir)
+        shutil.copytree(INSTALL_BUILDER_PROJECT_ROOT, components_dir)
 
         bundle_file = Path(shutil.copy("depsBundle.sh", f"{components_dir}/depsBundle.sh"))
         shutil.copy("depsBundle.py", f"{components_dir}/depsBundle.py")
@@ -224,7 +231,11 @@ def main(args: argparse.Namespace) -> None:
                     if args.install_builder_license_file != "NO_LICENSE"
                     else None
                 ),
-                install_builder_location=args.install_builder_location,
+                install_builder_location=(
+                    args.install_builder_location
+                    if args.install_builder_location
+                    else INSTALL_BUILDER["archive"]
+                ),
                 platform=args.platform,
                 local_dev_build=args.local_dev_build,
                 s3bucket=args.install_builder_s3_bucket,
@@ -296,8 +307,7 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--install-builder-location",
-        help="The InstallBuilder location, containing 'bin/builder'.",
-        required=True,
+        help="The InstallBuilder location, containing 'bin/builder'. Required for local dev builds.",
         type=Path,
     )
 
